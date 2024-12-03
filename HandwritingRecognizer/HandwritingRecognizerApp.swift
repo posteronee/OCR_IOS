36e09887a1db89888dbbf44371e8f9cc18783b92
posteronee <nikita2907200303@gmail.com>
2024-12-04 00:55:05 +0300

    Initial Commit

---
+++ HandwritingRecognizer/HandwritingRecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandwritingRecognizerApp.swift
+//  HandwritingRecognizer
+//
+//  Created by Никита Иванов on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HandwritingRecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
